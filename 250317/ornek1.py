4cea4e7e60f68ffd187524bdfed7f552edb6e3e1
sancarnisa <sncrnisa@gmail.com>
2025-03-17 12:01:09 +0300

    Add files via upload

---
+++ 250317/ornek1.py
@@ -0,0 +1,6 @@
+sayi1=(int(input("kullanicidan sayi1 degiskenini alin"))) #kullanıcıdan sayi1 degiskeni alınır
+sayi2=(int(input("kullanicidan sayi2 degiskenini alin"))) #kullanıcıdan sayi2 degiskeni alınır
+if sayi1>sayi2:  #eger sayi1 buyuk cıkarsa ekrana yazdırıldı
+    print("sayi1 büyüktür")
+else:
+    print("sayi1 büyük değil") #degil ise degildir yazdırılır
